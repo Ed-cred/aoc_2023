64e22e0d2f3d962ba582cab6f58b548d823d6312
Ed-cred <edwardandrew63@gmail.com>
2023-12-01 11:03:37 +0200

    Day1 solution c++

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -7,11 +7,12 @@ int getNum(const std::string line);
 int main(int argc, char **argv) {
     std::string line;
     std::ifstream myFile;
-    // std::cout << argv[1] << std::endl;
+    int sum = 0;
+    int num = 0;
     if (argc < 2) {
         std::cout << "Please provide a filename in the current directory to read" << std::endl;
     } else {
-         myFile.open(argv[1]);
+        myFile.open(argv[1]);
     }
 
     if (!myFile) {
@@ -20,19 +21,39 @@ int main(int argc, char **argv) {
 
     if (myFile.is_open()) {
         while (getline(myFile, line)) {
-            std::cout << line << '\n';
+            num = getNum(line);
+            std::cout << line << ": " << num << '\n';
+            sum += num;
         }
         myFile.close();
     } else {
         std::cout << "Unable to open file" << std::endl;
     }
+    std::cout << sum;
     return 0;
 }
 
-int getNum(const std::string line) {
-   for (char s : line) {
-        if (s >= 48 && s <= 57) {
-            
+int getNum(std::string line) {
+   int digit1 = -1;
+   int digit2 = -1;
+   int j = line.size() - 1;
+   int i = 0;
+    while (i <= j) {
+        if (digit1 == -1 || digit2 == -1) {
+            if (line[i] >= 48 && line[i] <= 57) {
+                digit1 = line[i] - '0';
+            } else {
+                i++;
+            }
+            if (line[j] >= 48 && line[j] <= 57) {
+                digit2 = line[j] - '0';
+            } else {
+                j--;
+            } 
+        } else {
+            break;
         }
-   } 
+    }
+    int result = (digit1 * 10) + digit2;
+    return result;
 }
