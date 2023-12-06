4027245c7c2b82236cfb9c6a1ff93d4f477a0f10
Ed-cred <edwardandrew63@gmail.com>
2023-12-06 14:07:45 +0200

    Day5 in progress

--- day5/day5.cpp
+++ day5/day5.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <unordered_map>
 
-int getSeeds (std::string &line); 
+int matchString(std::string &line); 
+void matchMap(std::string &line); 
 
 int main(int argc, char **argv) {
     std::string line;
@@ -18,9 +20,12 @@ int main(int argc, char **argv) {
         std::cout << "No such file in the directory\n";
     }
 
+
+    std::unordered_map<int,int> map;
     if (myFile.is_open()) {
         while (getline(myFile, line)) {
-            std::cout << line; 
+            std::cout << line << '\n'; 
+            matchString(line);
         }
         myFile.close();
     }
@@ -30,9 +35,18 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-int getSeeds (std::string &line) {
+int matchString (std::string &line, std::unordered_map<int, int> map) {
     int pos = line.find(':');
-    
+    std::string token = line.substr(0, pos);
+    if (token == "seeds") {
+        std::string seeds = line.substr(pos + 2, std::string::npos);
+        std::cout << "Seeds: " << seeds;
+    } else if(int pos = line.find(':'); pos + 1 == std::string::npos) {
+        map.clear();
+    }
     return 0;
+}
 
+void matchMap(std::string &line) {
+    
 }
