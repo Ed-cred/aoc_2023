c5785732645fc6da97af7ff2d13e17f7131f2a2b
Ed-cred <edwardandrew63@gmail.com>
2024-01-20 10:40:07 +0200

    Day 7 part 1 solution, counting the occurence of cards is ugly but it works for now

--- day7/day7p1.cpp
+++ day7/day7p1.cpp
@@ -1,130 +1,115 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
 #include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <string>
 #include <unordered_map>
+#include <vector>
 
 enum handType {
-    Five_kind = 5,
-    Four_kind = 4,
-    Full_house = 3,
-    Three_kind = 2,
-    Two_kind = 1,
-    High_card = 0,
+  Five_kind = 6,
+  Four_kind = 5,
+  Full_house = 4,
+  Three_kind = 3,
+  Two_kind = 2,
+  One_kind = 1,
+  High_card = 0,
 };
 
 struct Hand {
-    std::string cards;
-    int bid;
-    handType type;
+  std::string cards;
+  size_t bid;
+  handType type;
 };
 
-Hand matchHand(std::string &line); 
-// void sortRank(std::vector<Hand> &hand); 
+Hand matchHand(std::string& line);
 
 std::string labels = "AKQJT98765432";
 
 int main() {
-    std::string line;
-    std::ifstream myFile("test.txt");
-    std::vector<Hand> game;
-    size_t result = 0;
+  std::string line;
+  std::ifstream myFile("input.txt");
+  std::vector<Hand> game;
+  size_t result = 0;
 
-    if (myFile.is_open()) {
-        while (getline(myFile, line)) {
-            Hand currHand = matchHand(line);
-            game.push_back(currHand);
+  if (myFile.is_open()) {
+    while (getline(myFile, line)) {
+      Hand currHand = matchHand(line);
+      game.push_back(currHand);
+    }
+    myFile.close();
+  } else {
+    std::cout << "Unable to open file" << std::endl;
+  }
+
+  std::sort(game.begin(), game.end(), [](Hand a, Hand b) {
+    if (a.type == b.type) {
+      for (int j = 0; j < 5; j++) {
+        if (labels.find(a.cards[j]) < labels.find(b.cards[j])) {
+          return false;
+        } else if (labels.find(a.cards[j]) > labels.find(b.cards[j])) {
+          return true;
         }
-        myFile.close();
-    } else {
-        std::cout << "Unable to open file" << std::endl;
+      }
+    } else if (a.type > b.type) {
+      return false;
     }
-    
-     std::sort(game.begin(), game.end(), [](Hand a, Hand b) {
-        if (a.type == b.type) {
-         for (int j = 0; j < 5; j++) {
-            if (labels.find(a.cards[j]) < labels.find(b.cards[j])) {
-                return false;
-            } else if (labels.find(a.cards[j]) > labels.find(b.cards[j])) {
-                return true;
-            }
-         } 
-        } else if (a.type > b.type) {
-            return false;
-        }    
-        return true;
-     });
+    return true;
+  });
 
-    // sortRank(game);
-    for(int i = 0 ; i < game.size(); i++) {
-        std::cout << game[i].cards << "->" << game[i].type << "->" << game[i].bid << std::endl;
-        result += (game[i].bid * (i+1));
-    }
-    // aiming for this -> 254115617
-    std::cout << result << std::endl;
-    return 0;
+  size_t amount = 0;
+  for (int i = 0; i < game.size(); i++) {
+    amount = game[i].bid * (i + 1);
+    result += amount;
+  }
+  std::cout << result << std::endl;
+  return 0;
 }
 
-Hand matchHand(std::string &line) {
-   Hand currHand;
-   std::unordered_map<char, int> cardCount;
-   std::vector<int> occurence;
-   int pos = line.find(' ');
-   currHand.cards = line.substr(0, pos);
-   currHand.bid = stoi(line.substr(pos + 1, std::string::npos));
-   for(const char &c : currHand.cards) {
+Hand matchHand(std::string& line) {
+  Hand currHand;
+  std::unordered_map<char, int> cardCount;
+  std::vector<int> occurence;
+
+  int pos = line.find(' ');
+  currHand.cards = line.substr(0, pos);
+  currHand.bid = stoi(line.substr(pos + 1, std::string::npos));
+
+  for (const char& c : currHand.cards) {
     cardCount[c]++;
-   }
-   for (const auto [k,v] : cardCount) {
+  }
+  for (const auto [k, v] : cardCount) {
     occurence.push_back(v);
-   }
-   //this is so terribly janky it's fun
-   std::sort(occurence.begin(), occurence.end(), std::greater<int>());
-   for (int i = 0; i < occurence.size(); ++ i) {
-    if (occurence[i] == 5) {
+  }
+  //a better way to count the cards might be nice
+  std::sort(occurence.begin(), occurence.end(), std::greater<int>());
+  // this is so terribly janky it's fun
+  for (int i = 0; i < occurence.size(); ++i) {
+    switch (occurence[i]) {
+      case 5:
         currHand.type = Five_kind;
         break;
-    } else if (occurence[i] == 4) {
+      case 4:
         currHand.type = Four_kind;
         break;
-    } else if (occurence[i] == 3 && occurence[i+1] == 2 && i+1 < occurence.size()) {
-        currHand.type = Full_house;
-        break;
-    } else if (occurence[i] == 3 && occurence[i+1] == 1 && i+1 < occurence.size()) {
+      case 3:
+        if (i + 1 < occurence.size() && occurence[i + 1] == 2) {
+          currHand.type = Full_house;
+          break;
+        }
         currHand.type = Three_kind;
         break;
-    } else if (occurence[i] == 2) {
-        currHand.type = Two_kind;
+      case 2:
+        if (i + 1 < occurence.size() && occurence[i + 1] == 2) {
+          currHand.type = Two_kind;
+          break;
+        }
+        currHand.type = One_kind;
         break;
-    } else if (occurence[i] == 1) {
+      default:
         currHand.type = High_card;
-        break; 
+        break;
     }
-   }
-   return currHand;
+    break;
+  }
+  return currHand;
 }
-
-// probably should find a more efficient sort than this one 
-// void sortRank(std::vector<Hand> &hand) {
-//     bool ok = true;
-//     do {
-//         ok = true;
-//         for (int i = 0; i < hand.size(); ++i) {
-//             if(i+1 < hand.size() && hand[i].type == hand[i+1].type) {
-//                     for (int j = 0; j < 5; j++) {
-//                         if (labels.find(hand[i].cards[j]) < labels.find(hand[i+1].cards[j])) {
-//                             std::swap(hand[i], hand[i+1]);
-//                             ok = false;
-//                             break;
-//                         } else if (labels.find(hand[i].cards[j]) > labels.find(hand[i+1].cards[j])){
-//                             break;
-//                         }                 
-//                     }
-//             } else if (hand[i].type > hand[i+1].type && i + 1 < hand.size()) {
-//                 std::swap(hand[i], hand[i+1]);
-//                 ok = false;
-//             }
-//         }
-//     } while (!ok);
-// }
